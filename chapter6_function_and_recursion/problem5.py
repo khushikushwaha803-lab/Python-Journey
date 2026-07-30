f9167f375125f99a6030f300ee034153ba7422a1
Geetanjali <301621672+khushikushwaha803-lab@users.noreply.github.com>
2026-07-30 21:02:27 +0530

    added inches_to_cms code

---
+++ chapter6_function_and_recursion/problem5.py
@@ -0,0 +1,9 @@
+def inches_to_cms(inches):
+    return inches*2.54
+
+inches=int(input("enter the inches want to convert:"))
+print(f"{inches} inches is equal to :{inches_to_cms(inches)} cms")
+
+
+
+
