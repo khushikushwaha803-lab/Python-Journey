bbb76c4b87f3f9a3c2a81c1f7ee38e502f297d46
geetanjali0772 <geetanjali0772@gmail.com>
2026-08-07 22:58:50 +0530

    finding the squareroot of a number

---
+++ practice_questions.py/sqrt_of_num.py
@@ -0,0 +1,6 @@
+#finding the squareroot of a no
+import math
+num=int(input("enter the number: "))
+result=math.sqrt(num)
+# %d..%f these symbols are formative output symbol
+print("square root of %d = %.2f" %(num,result))
